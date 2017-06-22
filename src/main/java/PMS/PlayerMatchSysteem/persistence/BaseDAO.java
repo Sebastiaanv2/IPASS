da7d3fd3edd2c8e330bcedc75587cab36e187ff3
sebastiaan van Rijn <sebastiaan.94@live.nl>
2017-06-22 12:35:43 +0200

    removed obsolete methodes

--- src/main/java/PMS/PlayerMatchSysteem/persistence/BaseDAO.java
+++ src/main/java/PMS/PlayerMatchSysteem/persistence/BaseDAO.java
@@ -1,13 +1,14 @@
 package PMS.PlayerMatchSysteem.persistence;
 // Created on 1-6-2017.
 
+
 import java.net.URI;
 import java.sql.Connection;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 public class BaseDAO {
-    private static DataSource connectionPool;
+    private DataSource connectionPool;
     public BaseDAO() {
         try {
             final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
@@ -25,13 +26,13 @@ public class BaseDAO {
                 connectionPool = pool;
             } else {
                 InitialContext ic = new InitialContext();
-                connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/Postgres");
+                connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/PostgresDS");
             }
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-    protected static Connection getConnection() {
+    protected final Connection getConnection() {
         try {
             return connectionPool.getConnection();
         } catch (Exception ex) {
@@ -39,3 +40,42 @@ public class BaseDAO {
         }
     }
 }
+
+//import java.net.URI;
+//import java.sql.Connection;
+//import javax.naming.InitialContext;
+//import javax.sql.DataSource;
+//import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+//public class BaseDAO {
+//    private static DataSource connectionPool;
+//    public BaseDAO() {
+//        try {
+//            final String DATABASE_URL_PROP = System.getenv("DATABASE_URL");
+//            if (DATABASE_URL_PROP != null) {
+//                URI dbUri = new URI(DATABASE_URL_PROP);
+//                String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();
+//                BasicDataSource pool = new BasicDataSource();
+//                if (dbUri.getUserInfo() != null) {
+//                    pool.setUsername(dbUri.getUserInfo().split(":")[0]);
+//                    pool.setPassword(dbUri.getUserInfo().split(":")[1]);
+//                }
+//                pool.setDriverClassName("org.postgresql.Driver");
+//                pool.setUrl(dbUrl);
+//                pool.setInitialSize(1);
+//                connectionPool = pool;
+//            } else {
+//                InitialContext ic = new InitialContext();
+//                connectionPool = (DataSource) ic.lookup("java:comp/env/jdbc/Postgres");
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//    protected static Connection getConnection() {
+//        try {
+//            return connectionPool.getConnection();
+//        } catch (Exception ex) {
+//            throw new RuntimeException(ex);
+//        }
+//    }
+//}
